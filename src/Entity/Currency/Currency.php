2176e7c2f0b617163f3907317346f637b0e630d2
xEveryth1ngx <dobrowolski.piotrof@gmail.com>
2024-02-23 18:55:08 +0100

    Made Currency Entity

---
+++ src/Entity/Currency/Currency.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity\Currency;
+
+use App\Repository\Currency\CurrencyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
+class Currency
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $currencyCode = null;
+
+    #[ORM\Column]
+    private ?int $exchangeRate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currencyCode;
+    }
+
+    public function setCurrencyCode(string $currencyCode): static
+    {
+        $this->currencyCode = $currencyCode;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?int
+    {
+        return $this->exchangeRate;
+    }
+
+    public function setExchangeRate(int $exchangeRate): static
+    {
+        $this->exchangeRate = $exchangeRate;
+
+        return $this;
+    }
+}
