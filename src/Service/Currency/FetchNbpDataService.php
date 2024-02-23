3f25abc917d7247ba4bd95ae7a894edf398a1264
xEveryth1ngx <dobrowolski.piotrof@gmail.com>
2024-02-23 20:31:31 +0100

    Used custom exceptions, moved base part of the url to env

--- src/Service/Currency/FetchNbpDataService.php
+++ src/Service/Currency/FetchNbpDataService.php
@@ -2,7 +2,11 @@
 
 namespace App\Service\Currency;
 
+use App\Exception\NbpApiBadRequestException;
+use App\Exception\NbpApiResourceNotFoundException;
 use App\Interface\Currency\FetchApiDataInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -12,21 +16,29 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FetchNbpDataService implements FetchApiDataInterface
 {
+    private string $baseApiUrl;
+
     public function __construct(
         private readonly HttpClientInterface $client,
+        ParameterBagInterface $parameterBag,
     ) {
+        $this->baseApiUrl = $parameterBag->get('nbp.api.url');
     }
 
     /**
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws ClientExceptionInterface
+     * @throws NbpApiResourceNotFoundException
+     * @throws NbpApiBadRequestException
      */
     public function fetchApiData(): array
     {
-        $response = $this->client->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/a/?format=json');
+        $response = $this->client->request('GET', $this->baseApiUrl . '/exchangerates/tables/a/?format=json');
+
+        if ($response->getStatusCode() === Response::HTTP_NOT_FOUND) {
+            throw new NbpApiResourceNotFoundException('Resource not found.');
+        }
+        if ($response->getStatusCode() === Response::HTTP_BAD_REQUEST) {
+            throw new NbpApiBadRequestException('Bad request.');
+        }
 
         return $response->toArray();
     }
