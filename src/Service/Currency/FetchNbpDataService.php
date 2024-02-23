43bd4ece3645fc6731f5156a5830cbdde86bb2cc
xEveryth1ngx <dobrowolski.piotrof@gmail.com>
2024-02-23 19:37:26 +0100

    Added NBP data fetch service

--- src/Service/Currency/FetchNbpDataService.php
+++ src/Service/Currency/FetchNbpDataService.php
@@ -3,12 +3,31 @@
 namespace App\Service\Currency;
 
 use App\Interface\Currency\FetchApiDataInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FetchNbpDataService implements FetchApiDataInterface
 {
+    public function __construct(
+        private readonly HttpClientInterface $client,
+    ) {
+    }
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     public function fetchApiData(): array
     {
-        // TODO: Implement fetchApiData() method.
+        $response = $this->client->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/a/?format=json');
+
+        return $response->toArray();
     }
 }
