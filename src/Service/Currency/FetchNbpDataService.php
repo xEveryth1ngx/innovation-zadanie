d31ecdbdef7f22edc2c71f1eddd5ce2cd4f15ad3
xEveryth1ngx <dobrowolski.piotrof@gmail.com>
2024-02-23 19:13:57 +0100

    Added a FetchNbpDataService.php

---
+++ src/Service/Currency/FetchNbpDataService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service\Currency;
+
+use App\Interface\Currency\FetchApiDataInterface;
+
+class FetchNbpDataService implements FetchApiDataInterface
+{
+
+    public function fetchApiData(): array
+    {
+        // TODO: Implement fetchApiData() method.
+    }
+}
